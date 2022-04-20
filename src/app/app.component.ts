7a72b7dd9b37ccaa696213c74b92b089402cf537
ArthurSV <rthursv@gmail.com>
2022-04-20 14:36:14 -0300

    Enter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora';
+  
+  valorA: number=0;
+  valorB: number=0;
+  resultado:number=0;
+  
+  soma(){
+    this.resultado = this.valorA+this.valorB;
+  }
+  subtracao(){
+    this.resultado = this.valorA-this.valorB;
+  }
+  multiplicacao(){
+    this.resultado = this.valorA*this.valorB;
+  }
+  divisao(){
+    this.resultado= this.valorA/this.valorB
+  }
+  
+  
+
+
 }
